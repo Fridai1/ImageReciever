7e86d7de7acda37a597bd096b049fd064209d065
Nikolai <nikolaiviby3@gmail.com>
2019-09-02 17:49:39 +0200

    Initial commit

---
+++ ImageReciever/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageReciever
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udp = new UdpClient(6789);
+        IPEndPoint ep = new IPEndPoint(IPAddress.Any, 6789);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            byte[] imagebytearray = udp.Receive(ref ep);
+            Image picture = byteArrayToImage(imagebytearray);
+            pictureBox1.Image = picture;
+        }
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
